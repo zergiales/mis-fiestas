375c4421c97c45beef312183d95e05f089306dfc
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2023-09-12 00:02:15 +0200

    subida de back

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 import App from './App.vue';
 import router from './router';
-import vuetify from './plugins/vuetify'
-import store from './store'
+import vuetify from './plugins/vuetify';
+import store from './store';
 
 
 Vue.config.productionTip = false;
@@ -10,6 +11,7 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   vuetify,
+  Vuelidate,
   store,
   render: (h) => h(App)
 }).$mount('#app');
